6aa069d746d0cbaf2d62e0b2997ffaf57dc922b2
Jens Laufer <jenslaufer@gmail.com>
2017-07-24 13:08:44 +0200

    finalized course

--- p5/validation/validate_poi.py
+++ p5/validation/validate_poi.py
@@ -15,18 +15,46 @@ import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
-data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
 
-### first element is our labels, any added elements are predictor
-### features. Keep this the same for the mini-project, but you'll
-### have a different feature list when you do the final project.
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+
+from sklearn.metrics import confusion_matrix
+
+data_dict = pickle.load(
+    open("../final_project/final_project_dataset.pkl", "r"))
+
+# first element is our labels, any added elements are predictor
+# features. Keep this the same for the mini-project, but you'll
+# have a different feature list when you do the final project.
 features_list = ["poi", "salary"]
 
-data = featureFormat(data_dict, features_list, sort_keys = '../tools/python2_lesson13_keys.pkl')
+data = featureFormat(data_dict, features_list,
+                     sort_keys='../tools/python2_lesson13_keys.pkl')
 labels, features = targetFeatureSplit(data)
 
 
+# it's all yours from here forward!
 
-### it's all yours from here forward!  
+features_train, features_test, labels_train, labels_test = train_test_split(
+    features, labels, test_size=0.30, random_state=42)
 
+cls = DecisionTreeClassifier()
+
+cls.fit(features_train, labels_train)
+
+print len(features_test)
+print cls.score(features_test,  labels_test)
+
+labels_predicted = cls.predict(features_test)
+
+print confusion_matrix(labels_test, labels_predicted)
+print accuracy_score(labels_test, labels_predicted)
+print precision_score(labels_test, labels_predicted)
+print recall_score(labels_test, labels_predicted)
+print f1_score(labels_test, labels_predicted)
 
