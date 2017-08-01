3b7a7231258036c12d8e1d7a7292da1c321658b4
Jens Laufer <jenslaufer@gmail.com>
2017-08-01 10:20:42 +0200

    using pipelines now

--- p5/final_project/poi_id.py
+++ p5/final_project/poi_id.py
@@ -1,205 +1,38 @@
 #!/usr/bin/python
-
 import sys
+
 import pickle
-sys.path.append("../tools/")
 
-from feature_format import featureFormat, targetFeatureSplit
-from tester import dump_classifier_and_data
-
-from sklearn.cross_validation import train_test_split
-from time import time
-import pandas as pd
-from sklearn.metrics import precision_score
-from sklearn.metrics import recall_score
-from sklearn.metrics import precision_score
-from sklearn.metrics import fbeta_score
-from sklearn.metrics import f1_score
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import fbeta_score, make_scorer, recall_score, accuracy_score
-from sklearn.model_selection import cross_val_score
-
+import numpy as np
 
 import pandas as pd
-import seaborn as sb
-import math
+
 import matplotlib.pyplot as plt
 
-
+from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.svm import LinearSVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.decomposition import PCA
 from sklearn.feature_selection import SelectKBest
-from sklearn.feature_selection import chi2
-from sklearn.feature_selection import f_classif
-
-
 from sklearn.preprocessing import MinMaxScaler
-class FeatureScaler:
+from sklearn.metrics import fbeta_score, make_scorer, recall_score, accuracy_score
+from sklearn.feature_selection import chi2
 
-    def __init__(self, features):
-        self.__features = features
+sys.path.append("../tools/")
+from feature_format import featureFormat, targetFeatureSplit
 
-    def min_max_scale(self):
-        return MinMaxScaler().fit_transform(features)
-
-
-class FeatureSelector:
-
-    def __init__(self, features, labels, features_names, label_name):
-        self.__features = features
-        self.__labels = labels
-        self.__feature_names = features_names
-        self.__label_name = label_name
-        print self.__feature_names
-        print self.__label_name
-
-    def select_k_best(self, k=10):
-        kbest = SelectKBest(k=k)
-        fit = kbest.fit_transform(self.__features, self.__labels)
-        self.scores_ = kbest.scores_
-
-        results_list = zip(kbest.get_support(),
-                           self.__feature_names, kbest.scores_)
-        self.report_ = sorted(results_list, key=lambda x: x[2], reverse=True)
-
-        return fit
-
-
-class ClassifierTuner:
-
-    def __init__(self, classifier, features, labels, test_size,
-                 test_split_random_state, beta=1, param_grid=[{}], cv=3):
-        features_train, features_test, labels_train, labels_test = train_test_split(
-            features, labels, test_size=test_size, random_state=test_split_random_state)
-        self.classifier_ = classifier
-        self.__features_train = features_train
-        self.__labels_train = labels_train
-        self.__features_test = features_test
-        self.__labels_test = labels_test
-        self.__beta = beta
-        self.__name = type(classifier).__name__
-        self.__param_grid = param_grid
-        self.__cv = cv
-        self.__beta_scorer = make_scorer(fbeta_score, beta=self.__beta)
-
-        self.__tuning()
-
-    def __tuning(self):
-        print "tuning {}...".format(self.__name)
-
-        grid_search = GridSearchCV(
-            self.classifier_, self.__param_grid, scoring=self.__beta_scorer, cv=self.__cv)
-        grid_search.fit(self.__features_train, self.__labels_train)
-
-        self.tuning_report_ = grid_search.cv_results_
-        self.classifier_ = grid_search.best_estimator_
-        self.params_ = grid_search.best_params_
-        self.index_ = grid_search.best_index_
-        self.metrics_ = self.__metrics()
-
-        print "tuned."
-
-    def __metrics(self):
-        metrics_ = {}
-        predicted_test = self.classifier_.predict(self.__features_test)
-
-        metrics_['name'] = self.__name
-        metrics_['test_accuracy_score'] = round(accuracy_score(
-            self.__labels_test, predicted_test), 3)
-        metrics_['test_recall_score'] = round(recall_score(
-            self.__labels_test, predicted_test), 3)
-        metrics_['test_precision_score'] = round(precision_score(
-            self.__labels_test, predicted_test), 3)
-        metrics_['test_f{}_score'.format(self.__beta)] = round(fbeta_score(
-            self.__labels_test, predicted_test, self.__beta), 3)
-
-        vals = cross_val_score(
-            self.classifier_, self.__features_train, self.__labels_train, scoring='accuracy', cv=self.__cv)
-        metrics_['cv_train_accuracy_score_mean'] = round(vals.mean(), 3)
-        metrics_['cv_train_accuracy_score_std'] = round(vals.std(), 3)
-
-        vals = cross_val_score(
-            self.classifier_, self.__features_train, self.__labels_train, scoring='precision', cv=self.__cv)
-        metrics_['cv_train_precision_score_mean'] = round(vals.mean(), 3)
-        metrics_['cv_train_precision_score_std'] = round(vals.std(), 3)
-
-        vals = cross_val_score(
-            self.classifier_, self.__features_train, self.__labels_train, scoring='recall', cv=self.__cv)
-        metrics_['cv_train_recall_score_mean'] = round(vals.mean(), 3)
-        metrics_['cv_train_recall_score_std'] = round(vals.std(), 3)
-
-        vals = cross_val_score(
-            self.classifier_, self.__features_train, self.__labels_train, scoring=self.__beta_scorer, cv=self.__cv)
-        metrics_['cv_f{}_beta_mean'.format(
-            self.__beta)] = round(vals.mean(), 3)
-        metrics_['cv_f{}_beta_std'.format(
-            self.__beta)] = round(vals.std(), 3)
-
-        return metrics_
-
-
-class Plot:
-
-    def __init__(self, data_dict):
-        data = []
-        for key, value in data_dict.iteritems():
-            value['name'] = key
-            data.append(value)
-        self.df_ = pd.DataFrame(data)
-        self.df_.replace({'NaN': 0}, inplace=True)
-
-    def boxplot(self, column, ax=None):
-        sb.boxplot(data=self.df_, x='poi', y=column, ax=ax)
-
-        head = self.df_.sort_values(by=[column], ascending=[False]).head(10)
-        tail = self.df_.sort_values(by=[column], ascending=[False]).tail(10)
-
-        def ann(row):
-            ind = row[0]
-            r = row[1]
-            plt.annotate(r['name'], xy=(r["poi"], r[column]),
-                         xytext=(2, 2), textcoords="offset points")
-
-        for row in head.iterrows():
-            ann(row)
-        for row in tail.iterrows():
-            ann(row)
-        if ax == None:
-            plt.show()
-
-    def boxplots(self):
-        for feature in list(self.df_.columns):
-            fig, ax = plt.subplots(1, 1)
-            if feature != 'name' and feature != 'email_address':
-                self.boxplot(feature, ax)
-        plt.show()
-
-    def boxplot_grid(self):
-
-        features = list(self.df_.columns)
-        features.remove('name')
-        features.remove('email_address')
-        features.remove('poi')
-
-        height = int(math.ceil(len(features) / 2))
-
-        fig = plt.figure()
-        fig.set_figheight(15 * height)
-        fig.set_figwidth(15)
-
-        axes = []
-        for i in range(0, height):
-            for j in range(0, 2):
-                axes.append(plt.subplot2grid((height, 2), (i, j)))
-
-        for feature in features:
-            boxplot(self.df_, feature, ax=axes[features.index(feature)])
-
-        plt.show()
+from tester import dump_classifier_and_data, load_classifier_and_data, test_classifier
 
 # Task 1: Select what features you'll use.
 # features_list is a list of strings, each of which is a feature name.
 # The first feature must be "poi".
- # You will need to use more features
-
 features_list = ['poi', 'bonus', 'deferral_payments', 'deferred_income', 'director_fees',
                  'exercised_stock_options', 'expenses',
                  'from_messages', 'from_poi_to_this_person',
@@ -208,41 +41,23 @@ features_list = ['poi', 'bonus', 'deferral_payments', 'deferred_income', 'direct
                  'restricted_stock_deferred', 'salary', 'shared_receipt_with_poi',
                  'to_messages', 'total_payments', 'total_stock_value']
 
-
 # Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 
-
 # Task 2: Remove outliers
-
-my_dataset = data_dict
-
-
-my_dataset.pop('TOTAL')
-my_dataset.pop('THE TRAVEL AGENCY IN THE PARK')
-
+data_dict.pop('TOTAL')
+data_dict.pop('THE TRAVEL AGENCY IN THE PARK')
+data_dict.pop('LOCKHART EUGENE E')
 
 # Task 3: Create new feature(s)
-# Store to my_dataset for easy export below.
 
-data = featureFormat(my_dataset, features_list, sort_keys=True)
-labels, features = targetFeatureSplit(data)
-
-features = FeatureScaler(features).min_max_scale()
-
-
-# Plot(data_dict).boxplots()
+# Store to data_dict for easy export below.
+my_dataset = data_dict
 
 # Extract features and labels from dataset for local testing
-
-
-
-selector = FeatureSelector(features, labels,
-                           features_list[1:], features_list[0])
-features = selector.select_k_best()
-
-print selector.report_
+data = featureFormat(my_dataset, features_list, sort_keys=True)
+labels, features = targetFeatureSplit(data)
 
 
 # Task 4: Try a varity of classifiers
@@ -251,10 +66,6 @@ print selector.report_
 # you'll need to use Pipelines. For more info:
 # http://scikit-learn.org/stable/modules/pipeline.html
 
-# Provided to give you a starting point. Try a variety of classifiers.
-from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVC
-
 # Task 5: Tune your classifier to achieve better than .3 precision and recall
 # using our testing script. Check the tester.py script in the final project
 # folder for details on the evaluation method, especially the test_classifier
@@ -262,18 +73,89 @@ from sklearn.svm import SVC
 # stratified shuffle split cross validation. For more info:
 # http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
-# Example starting point. Try investigating other evaluation techniques!
+
+pipe = Pipeline([
+    ('scale', None),
+    ('reduce_dim', None),
+    ('classify', None)
+])
 
 
-clf = ClassifierTuner(GaussianNB(), features, labels, 0.3, 42)
-print clf.metrics_
-print clf.params_
-print clf.tuning_report_
+scale_params = {'scale': [MinMaxScaler()]}
 
+pca_params = {
+    'reduce_dim': [PCA(iterated_power=7)],
+    'reduce_dim__n_components': [0.3, 0.4, 0.5, 0.6, 0.7],
+    'reduce_dim__whiten': [False],
+    'reduce_dim__copy': [True, False]
+}
+
+k_best_params = {
+    'reduce_dim': [SelectKBest()],
+    'reduce_dim__k': list(range(2, 10, 1))}
+
+gaussian_nb_params = {
+    'classify': [GaussianNB()]
+}
+
+svc_params = {
+    'classify': [SVC()],
+    'classify__kernel': ['rbf', 'linear', 'poly'],
+    'classify__C': [1, 5, 10, 50, 100, 200, 500],
+    'classify__gamma': list(np.arange(0.1, 0.9, 0.1))
+}
+
+random_forest_params = {
+    'classify': [RandomForestClassifier()],
+    'classify__criterion': ['gini', 'entropy'],
+    'classify__min_samples_split': [10, 15, 20, 25]
+}
+
+decision_tree_params = {
+    'classify': [DecisionTreeClassifier()],
+    'classify__criterion': ['gini', 'entropy'],
+    'classify__min_samples_split': [10, 15, 20, 25]
+}
+
+logistic_regression_params = {
+    'classify': [LogisticRegression()],
+    "classify__C": [0.05, 0.5, 1, 10, 10**2, 10**3, 10**5, 10**10, 10**15],
+    "classify__tol": [10**-1, 10**-2, 10**-4, 10**-5, 10**-6, 10**-10, 10**-15],
+    "classify__class_weight": ['balanced']
+}
+
+
+classifier_params = [gaussian_nb_params,
+                     svc_params,
+                     decision_tree_params,
+                     random_forest_params,
+                     logistic_regression_params]
+reducer_params = [pca_params, k_best_params]
+
+param_grid = []
+
+for classifier_param in classifier_params:
+    for reducer_param in reducer_params:
+        params = dict(scale_params.items() +
+                      reducer_param.items() + classifier_param.items())
+        param_grid.append(params)
+
+
+grid = GridSearchCV(pipe, param_grid=param_grid,
+                    scoring=make_scorer(fbeta_score, beta=1))
+
+
+from sklearn.cross_validation import train_test_split
+features_train, features_test, labels_train, labels_test = \
+    train_test_split(features, labels, test_size=0.3, random_state=42)
+
+grid.fit(features_train, labels_train)
+
+clf = grid.best_estimator_
 
 # Task 6: Dump your classifier, dataset, and features_list so anyone can
 # check your results. You do not need to change anything below, but make sure
 # that the version of poi_id.py that you submit can be run on its own and
 # generates the necessary .pkl files for validating your results.
 
-dump_classifier_and_data(clf.classifier_, my_dataset, features_list)
+dump_classifier_and_data(clf, my_dataset, features_list)
